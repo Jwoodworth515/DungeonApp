52b0a1193acae8712462dff6d15dcb92c325557d
Jacob Woodworth <jwoodworth515@gmail.com>
2021-05-30 13:09:20 -0500

    StereoType Enum Commit

---
+++ ClassLibrary/Stereotype.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public enum Hero
+    {
+        TheHipster,
+        TheSuperFan,
+        TheDancer,
+        MerchHound,
+        TheGiant,
+        TheVIP,
+        TheRaver
+    }
+    public enum Villian
+    {
+        TheScalper,
+        TheHeckler,
+        FunPolice,
+        TheTrainwreck,
+        TheBro,
+        MudPeople,
+        TheHippie
+    }
+}
---
+++ Dungeon/AppNotes.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon
+{
+    class AppNotes
+    {
+
+        //This space will store my wire frame and ideas as I progress through this project
+
+        /*
+         * Dungeon Application Wire Frame
+
+Dungeon Working Title: The Music Pit
+Player Type (Concert Goer) / Weapon of Choice
+	1. Hipster (Fanny Pack) Upper Level
+	2. The Super Fan (Sharpie Marker) Mid Level
+	3. The Dancer (Sick Moves) Low level
+	4. Merch Hound (Tour Poster) Mid Level
+	5. The Giant (Stoic Pose) Low Level
+	6. The VIP (Lanyard) Mid Level
+	7. The Raver (Glow Sticks) Low level
+	
+Monster Type (Obstacles / Weapon of Choice
+	1. The Scalper (Rubber Bands) (Depending on players health) Low to Upper Level
+	2. The Heckler (Harsh Words) Low level
+	3. Fun Police (Flash Light) Upper Level
+	4. The Trainwreck (Hysterical Tears) (My version of the Vampire) Mid to Upper Level
+	5. The Bro (Fist Pump) Mid Level
+	6. Mud People (Mud Sludge) Mid Level
+	7. The Hippie (Hula Hoop)Low level
+	
+Dungeon Rooms
+	1. Bathroom - "A wall that holds a seat with a hole in it is in this chamber. It's a privy! The noisome stench from the hole leads you to believe that the privy sees regular use.", "Stinking smoke wafts up from braziers made of skulls set around the edges of this room. The walls bear scratch marks and lines of soot that form crude pictures and what looks like words in some strange language.
+	2. Concession Line -  Neither light nor dark can penetrate the gloom in this chamber. An unnatural shade fills it, and the room's farthest reaches are barely visible. Lining the pillars are skeletons of all shapes and sizes.  It's as if anyone that enters this room, never makes the journey to the other end.  At the far end you can just barely perceive a lump about the size of a human lying on the floor. (It might be a dead body, a pile of rags, or a sleeping monster that can take advantage of the room's darkness.)
+	3. The Pit - This room makes everyone entering it feel claustrophobic. The walls feel like they are constantly closing in, and ceiling looks menacing.  The energy here amplifies the deafening noise as you gasp for air.
+	4. Parking Lot -  A chill wind blows against you as you open the door. Beyond it, you see that the floor and ceiling are nothing but iron grates. Above and below the grates the walls extend up and down with no true ceiling or floor within your range of vision. It's as though the chamber is a bridge through the shaft of a great well. Standing on the edge of this shaft, you feel a chill wind pass down it and over your shoulder into the hall beyond.
+	5. Backstage (Reward Room) entering this room raises your heartbeat to an unnatural level.  The smells are enchanting and the rows of tables host mountains of provisions. 
+	6. Green Room This small chamber resembles nothing that the name suggests.  It seems to have the remnants of a feast made for an army that has been picked clean.  The pungent smell of dried sweat and shame is almost unbearable.  An open curtain at the end of the room reveals what seems to be extension of the room.  With the lights flickering a shadowy figure approaches.
+	7. Balcony - There are strange faraway sounds of people talking in this room. The sounds can-not be deciphered. The room is completely empty.
+	8. Stage - (Reward Room) - This room is filled with the most brilliant colors followed by total and complete darkness. Between the flashing of lights and the black are stars. Millions of stars. Stepping into the room one gets the uncanny impression of floating in deep space.   
+	9. General Admission Area - Stepping into this room is like entering a desert during a high sun. The walls are a faded yellow, and the floor has a sand like pattern. The ceiling is a bright white. Staying in this room for longer than 5 minutes makes you feel parched.
+	10. Front Row - This room is a raging inferno of fire. It isn't magical, and it will hurt. The magical effect of the room is in keeping the fire contained inside it. Carefully and slowly walking into the room one will realize that there is a safe narrow path inside which will take you to the other exit(s).  There is only one thing standing between you and the promise of safer harbors.
+
+In Game Ideas
+	- Option to play as either a monster or a hero.  All Characters that are not chosen by the player automatically become monsters.
+	- Music for damage dealt
+    -screen flash for damage received
+	- Alert Player of Health depletion with audible alert
+	- Don't forget about the sleep function
+    -ASCII art, skull for you died page
+    -Treasure Chest for Reward Rooms
+    -Multiple reward rooms to encourage lengthier gameplay
+    - Train Wreck will work off of date time, the hours of 8pm-4am are her time to shine
+    -When a upper level monster is defeaed, health is gained
+    -Running away cost health points
+    -ask the magic 8 ball advice on to run or stay
+    -
+         * 
+         */
+
+    }
+}
